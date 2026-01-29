69d6e15aa7195d6cc9f7517702f541de56ec8e68
jawed <jawadtouir03@gmail.com>
2026-01-29 21:21:28 +0100

    commit2

---
+++ Lesson03/meaning.py
@@ -0,0 +1,10 @@
+meaning = 9
+print("")
+
+# if meaning > 10:
+#     print("right on!")
+# else:
+#     print("not today")
+
+# Ternary Operator
+print("Right on!") if meaning > 10 else print("Not Today!")
