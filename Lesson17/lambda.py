1e71ac42e47695dcc34f1ef399590e42d609d14a
jawed <jawadtouir03@gmail.com>
2026-02-12 22:02:16 +0100

    Lesson17

--- Lesson17/lambda.py
+++ Lesson17/lambda.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def squared(num):
     return num * num
 
@@ -7,8 +10,8 @@ print(squared(2))
 addTwo = lambda num: num + 2
 print(addTwo(12))
 
-sum = lambda a, b: a + b
-print(sum(5, 3))
+sum_total = lambda a, b: a + b
+print(sum_total(5, 3))
 
 
 #########################
@@ -27,3 +30,24 @@ print(add_twenty(7))
 numbers = [1, 2, 3, 22, 44, 7]
 squared_nums = map(lambda num: num * num, numbers)
 print(list(squared_nums))
+
+
+##################
+# Filter
+
+
+odd_nums = filter(lambda num: num % 2 != 0, numbers)
+print(list(odd_nums))
+
+#############
+
+numbers = [1, 2, 3, 4, 5, 1]
+total = reduce(lambda acc, curr: acc + curr, numbers, 10)
+print(total)
+
+print(sum(numbers))
+
+
+names = ["dave gray", "sara ito", "john jacob", "jingleheimerschmidt"]
+char_count = reduce(lambda acc, curr: acc + len(curr), names, 0)
+print(char_count)
